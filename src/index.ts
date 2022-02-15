7d35a95f93818ada3e40a42c5380b901547800cd
linusyh <linus.leong.yh@gmail.com>
2022-02-15 12:16:41 +0000

    Fix package.json dependency, build command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 require('dotenv').config({ path: __dirname+'/.env' });
 console.log(process.env.JS_API_KEY)
 
-import {runThisFunction as cloudFunction} from "./functions/dummyDataServer";
-
-export {cloudFunction};
+export {runThisFunction as cloudFunction} from "./functions/dummyDataServer";

6113ff13a67887a827d53e6e94e56c3da9358598
linusyh <linus.leong.yh@gmail.com>
2022-02-16 03:12:42 +0000

    Implements communication with Google Directions API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-require('dotenv').config({ path: __dirname+'/.env' });
-console.log(process.env.JS_API_KEY)
-
-export {runThisFunction as cloudFunction} from "./functions/dummyDataServer";
+export {runThisFunction as cloudFunction} from "./functions/simpleServer1";
